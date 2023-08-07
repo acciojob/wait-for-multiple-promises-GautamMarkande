edd1d32bb9aaf9ada1fcc3a48582dabb04c5dfe5
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2023-08-07 12:39:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//your JS code here. If required.
+let a = new Promise((resolve)=>{
+	setTimeout(()=>{
+		resolve(1);
+	},2000);
+})
+let b = new Promise((resolve)=>{
+    setTimeout(()=>{
+        resolve(1)
+    },1000)
+});
+let c = new Promise((resolve)=>{
+    setTimeout(() => {
+        resolve(3);
+    }, 3000);
+})
+let x = Promise.all([a,b,c]);
+const tbody = document.querySelector("tbody");
+x.then((arr)=>{
+   let totaltime = 0;
+   for(let i = 0; i<arr.length; i++){
+    totaltime=Math.max(arr[i],totaltime);
+	const tr = document.createElement("tr");
+ 	tr.innerHTML = `<td>Promise ${i+1}</td>
+           <td>${arr[i]}</td>
+ 		`
+ 	tbody.appendChild(tr);
+   }
+   const tr = document.createElement("tr");
+ 	tr.innerHTML = `<td>Total</td>
+           <td>${totaltime}</td>
+ 		`
+ 	tbody.appendChild(tr);
+   tbody.firstElementChild.remove();
+})
